5d33488aca41c330f57a84f3c1d3344dc6d3b9b8
ali el ouardi <alielouardi2@gmail.com>
2016-11-13 19:53:47 +0100

    init project spring boot SINFO

---
+++ src/main/java/org/sinfo/SinfoApplication.java
@@ -0,0 +1,12 @@
+package org.sinfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SinfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SinfoApplication.class, args);
+	}
+}
