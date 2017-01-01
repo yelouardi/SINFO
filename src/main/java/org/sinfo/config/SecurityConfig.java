370ce7e124c838d97517dd101175d6bcd317cd1e
yelouardi <ing.elouardi@gmail.com>
2017-01-01 20:09:36 +0100

    config spring security

--- src/main/java/org/sinfo/config/SecurityConfig.java
+++ src/main/java/org/sinfo/config/SecurityConfig.java
@@ -2,19 +2,14 @@ package org.sinfo.config;
 
 import org.sinfo.security.auth.CustomerAuthService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.security.SecurityProperties;
-import org.springframework.context.annotation.Bean;
-import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.session.web.http.HeaderHttpSessionStrategy;
 
 /**
  * @author yelouardi SecurityConfig
  */
-@Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Autowired
@@ -36,9 +31,5 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     auth.userDetailsService(userService);
   }
 
-  @Bean
-  public HeaderHttpSessionStrategy sessionStrategy() {
-    return new HeaderHttpSessionStrategy();
-  }
 
 }
