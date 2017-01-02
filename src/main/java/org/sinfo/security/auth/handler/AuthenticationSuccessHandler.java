86c7de1b853aa0fdb9e2ce0e7b5a2bd9e64103be
yelouardi <ing.elouardi@gmail.com>
2017-01-02 10:23:51 +0100

    config spring security

--- src/main/java/org/sinfo/security/auth/handler/AuthenticationSuccessHandler.java
+++ src/main/java/org/sinfo/security/auth/handler/AuthenticationSuccessHandler.java
@@ -3,15 +3,16 @@ package org.sinfo.security.auth.handler;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.stream.Collectors;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.log4j.Logger;
 import org.sinfo.security.auth.bo.SecurityUser;
 import org.sinfo.security.auth.dto.UserDto;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 /**
@@ -19,14 +20,12 @@ import com.fasterxml.jackson.databind.ObjectMapper;
  * AuthenticationSuccessHandler
  */
 public class AuthenticationSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
-private static final Logger LOGGER=Logger.getLogger(AuthenticationSuccessHandler.class);
 	@Override
 	public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws ServletException, IOException {
 		SecurityUser userSecurity =  (SecurityUser) authentication.getPrincipal();
 		ObjectMapper mapper = new ObjectMapper();
 		PrintWriter writer = response.getWriter();
-		UserDto userDto = new UserDto(request.getSession(false).getId(),userSecurity.getUserNo(), userSecurity.getUsername(), userSecurity.getAuthorities().stream().map(x -> x.getAuthority()).collect(Collectors.toList()));
-		LOGGER.info("------------>>  HEADER :"+userSecurity.toString());
+		UserDto userDto = new UserDto("",userSecurity.getUserNo(), userSecurity.getUsername(), userSecurity.getAuthorities().stream().map(x -> x.getAuthority()).collect(Collectors.toList()));
 		mapper.writeValue(writer, userDto);
 		writer.flush();
 	}
