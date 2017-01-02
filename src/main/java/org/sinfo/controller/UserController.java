456fa5fb77614f88fb2a2ad56d16fe047e053059
yelouardi <ing.elouardi@gmail.com>
2017-01-02 09:40:30 +0100

    config spring security

--- src/main/java/org/sinfo/controller/UserController.java
+++ src/main/java/org/sinfo/controller/UserController.java
@@ -1,9 +1,15 @@
 package org.sinfo.controller;
 
+import java.util.Collections;
+import java.util.Map;
+
+import javax.servlet.http.HttpSession;
+
 import org.sinfo.security.auth.AuthService;
 import org.sinfo.security.auth.dto.UserDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -21,4 +27,9 @@ public class UserController {
         return authService.getLoggedUser();
     }
 
+    @RequestMapping("/token")
+    @ResponseBody
+    public Map<String,String> token(HttpSession session) {
+      return Collections.singletonMap("token", session.getId());
+    }
 }	
