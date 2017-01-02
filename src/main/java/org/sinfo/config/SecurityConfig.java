bf1c50d7bd85cf3b592ed93dc7e7406d9ad39193
yelouardi <ing.elouardi@gmail.com>
2017-01-02 11:10:56 +0100

    config spring security

--- src/main/java/org/sinfo/config/SecurityConfig.java
+++ src/main/java/org/sinfo/config/SecurityConfig.java
@@ -13,6 +13,7 @@ import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.session.web.http.HeaderHttpSessionStrategy;
 import org.springframework.session.web.http.HttpSessionStrategy;
@@ -66,7 +67,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.exceptionHandling().authenticationEntryPoint(restAuthenticationEntryPoint())
 		.and().addFilterBefore(authenticationFilter(), CustomUsernamePasswordAuthenticationFilter.class)
-		.csrf().disable().authorizeRequests().antMatchers("/token").permitAll().antMatchers("/**").authenticated().and().formLogin()
+		.authorizeRequests().antMatchers("/token").permitAll().antMatchers("/**").authenticated()
+		.and().csrf()
+        .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()).and().formLogin()
 		.loginProcessingUrl(AUTHENTICATE_ENDPOINT).failureHandler(authenticationFailureHandler())
 		.successHandler(authenticationSuccessHandler()).and().logout();
     }
